a55f74a40d46ec49b3b02d45d56b61e0aa76f9f2
Diana Cardona <dscardona@gmail.com>
2023-05-15 09:08:35 -0400

    Add logos and styling to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,19 @@
+import twitter from "../../public/images/twitter.png"
+import facebook from "../../public/images/facebook.png"
+import instagram from "../../public/images/instagram.png"
+import github from "../../public/images/github.png"
+
+
 function Footer() {
     return (
-        <h1>This is the footer section</h1>
+        <div className="footer">
+            <div className="footer-social-logos">
+                <img src={twitter} alt="Twitter logo"/>
+                <img src={facebook} alt="Facebook logo"/>
+                <img src={instagram} alt="Instagram logo"/>
+                <img src={github} alt="Github logo"/> 
+            </div>
+        </div>
     )
 }
 
