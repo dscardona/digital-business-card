e23ade4f54a95c335cdb2278f20fc7c4907e40b3
Diana Cardona <dscardona@gmail.com>
2023-05-14 20:58:44 -0400

    Styled About/Interest components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,10 @@
 function About() {
     return (
         <>
-        
-        <h1>This is the about section</h1>
+        <div className="about-body">
+            <h3>About</h3>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        </div>
         </>
     )
 }
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,6 +1,11 @@
 function Interests() {
     return (
-        <h1>This is the interests section</h1>
+        <>
+        <div className="about-body">
+            <h3>Interests</h3>
+            <p>Before working as a developer, I worked as an interpreter for the courts of the State of New York. I'm curious about languages, natural and otherwise. On my free time you can find me reading, doing yoga, baking bread, finding efficiencies. I enjoy dancing and cooking for friends and family.</p>
+        </div>
+        </>
     )
 }
 
