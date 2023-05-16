9db9210756596f653c668948639dabadc6de6180
Diana Cardona <dscardona@gmail.com>
2023-05-16 11:25:50 -0400

    Additional adjustments of About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
         <>
         <div className="about-body">
             <h3>About</h3>
-            <p>I'm an aspiring full-stack web developer with experience in a variety of technologies, including HTML, CSS, JavaScript, React, Node.js, and MongoDB, SQL, and Python. I am always looking for new challenges and opportunities to learn and grow.</p>
+            <p>I'm an aspiring full-stack web developer with experience in a variety of technologies, including HTML, CSS, JavaScript, React, Node.js, MongoDB, SQL, Flask and Python. I am always looking for new challenges and opportunities to learn and grow.</p>
         </div>
         </>
     )
