56c0133ac296bef247c5a9289c922086d2577bfa
Diana Cardona <dscardona@gmail.com>
2023-05-16 11:34:23 -0400

    Edit content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
         <>
         <div className="about-body">
             <h3>About</h3>
-            <p>I'm an aspiring full-stack web developer with experience in a variety of technologies, including HTML, CSS, JavaScript, React, Node.js, MongoDB, SQL, Flask and Python. I am always looking for new challenges and opportunities to learn and grow.</p>
+            <p>I'm an aspiring full-stack web developer with experience in a variety of technologies, including HTML, CSS, JavaScript, React, Node.js, MongoDB, SQL, Flask and Python. A couple of things I take pride in are my work ethic, and the polished work I always strive to produce.</p>
         </div>
         </>
     )
